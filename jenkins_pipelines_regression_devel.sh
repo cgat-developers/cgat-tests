bcea0be1587d2c24baaed4c45e4a5fe273edc60c
Andreas Heger <andreas.heger@gmail.com>
2019-02-13 20:56:08 +0000

    {AH] new install.sh options

--- jenkins_pipelines_regression_devel.sh
+++ jenkins_pipelines_regression_devel.sh
@@ -39,7 +39,7 @@ ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ${SUBMIT_HOS
     export TERM=xterm && \
     bash install-devel.sh \
     --install \
-    --with-pipelines \
+    --install-pipelines-dependencies \
     --use-repo ${WORKSPACE} \
     --location ${WORKSPACE}/cgat-developers"
 
